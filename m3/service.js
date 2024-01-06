7b92b51d283e0485c9cb39b5fe9011132ce40324
José Eduardo Rocha Peres <joseeduardorperes@gmail.com>
2024-01-06 16:04:09 -0300

    feat: criando testes com mocha

---
+++ m3/service.js
@@ -0,0 +1,20 @@
+const { get } = require('axios');
+const URL = 'https://swapi.dev/api/people';
+
+async function obterPessoas(nome) {
+	const url = `${URL}/?search=${nome}`;
+	const response = await get(url);
+
+	return response.data.results.map(mapearPessoas);
+}
+
+function mapearPessoas(item) {
+	return {
+		nome: item.name,
+		peso: item.mass,
+	};
+}
+
+module.exports = {
+	obterPessoas,
+};
